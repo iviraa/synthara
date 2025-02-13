bf5fe8ba497c8e5df43756e556db9a3e70e8045f
iviraa <chetanchal.cs@gmail.com>
2025-02-13 15:34:20 -0600

    setup prisma

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
@@ -8,6 +9,26 @@ export const appRouter = router({
 
     if (!(await user).id || !(await user).email)
       throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const dbUser = await db.user.findFirst({
+      where: { id: (await user).id },
+    });
+
+    if (!dbUser) {
+      const userId = (await user).id;
+      const userEmail = (await user).email;
+
+      if (!userEmail) {
+        throw new Error("User email is null or undefined.");
+      }
+
+      await db.user.create({
+        data: {
+          id: userId,
+          email: userEmail,
+        },
+      });
+    }
 
     return { success: true };
   }),
